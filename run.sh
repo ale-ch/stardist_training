f2ad1944fe4a393cd8341d6eb432bb80c2ab9acd
Chiodin Alessio <ieo7086@cn14.cluster.loc>
2025-04-30 11:35:30 +0200

    Saving config

--- run.sh
+++ run.sh
@@ -1,12 +1,25 @@
-singularity_imgs_dir=/hpcnfs/scratch/DIMA/chiodin/tests/stardist_training_notebook/singularity_images
+singularity_imgs_dir=/hpcnfs/scratch/DIMA/chiodin/singularity_images
 base_dir=/hpcnfs/scratch/DIMA/chiodin/tests/stardist_training_notebook/tests/test_imaging_data
 scripts_dir=/hpcnfs/scratch/DIMA/chiodin/tests/stardist_training_notebook/stardist_training
 model_name=stardist
+model_dir=${base_dir}/models/${model_name}
 
 epochs=5
 steps_per_epoch=5
 val_prop=0.1
 val_prop_opt=1
+
+# Save to text file
+echo "Saving training configuration to ${model_dir}/training_conf.txt"
+cat <<EOF > ${model_dir}/training_conf.txt
+model_name=$model_name
+epochs=$epochs
+steps_per_epoch=$steps_per_epoch
+val_prop=$val_prop
+val_prop_opt=$val_prop_opt
+EOF
+
+
 
 # Run training
 singularity exec \
