22fc72704b0c279376cacbb95136606467d4d0fc
Alessio Chiodin <ieo7086@Chiodins-MacBook-Air.local>
2025-05-02 09:52:33 +0200

    Updated docker build script

--- docker/stardist_tf2/build_example.sh
+++ docker/stardist_tf2/build_example.sh
@@ -1,4 +1,4 @@
-docker buildx build --platform linux/amd64 -t alech00/stardist_training:v0.0 --push .
+docker buildx build --platform linux/amd64 -t alech00/stardist_training:v0.1 --push .
 
-singularity build stardist_training.sif docker://alech00/stardist_training:v0.0
+singularity build stardist_training.sif docker://alech00/stardist_training:v0.1
 
