c87083af3101813cb378bb3faebf5b39c5d7299d
Chiodin Alessio <ieo7086@cn13.cluster.loc>
2025-05-05 12:05:46 +0200

    added example and source for pretrained model loading

--- utils/conf_model.py
+++ utils/conf_model.py
@@ -50,3 +50,34 @@ def instantiate_model(conf, basedir, model_name, pretrained=True):
 
 
     return model
+
+
+#### Example ####
+
+# https://forum.image.sc/t/how-to-continue-training/73482/5
+# import numpy as np
+# from stardist.models import StarDist2D
+# import shutil 
+# 
+# # make a copy of a pretrained model into folder 'mymodel'
+# model_pretrained = StarDist2D.from_pretrained('2D_versatile_fluo')
+# shutil.copytree(model_pretrained.logdir, 'mymodel', dirs_exist_ok=True)
+# 
+# 
+# # create new model from folder (loading the  pretrained weights)
+# model = StarDist2D(None, 'mymodel')
+# 
+# # create new training data 
+# X = np.zeros((16,128,128,1))
+# X[:, 10:20,10:20] = 1.1
+# Y = np.zeros((16,128,128), np.uint16)
+# Y[:, 10:20,10:20] = 1
+# 
+# # change some training params 
+# model.config.train_patch_size = (128,128)
+# model.config.train_batch_size = 16 
+# model.config.train_learning_rate = 1e-5
+# model.config.train_epochs = 10
+# 
+# # finetune on new data
+# model.train(X,Y, validation_data=(X,Y))
