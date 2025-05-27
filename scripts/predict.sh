0cf48c9bcc19be82ae5c5ee19b8f8a9aa7d71d87
Chiodin Alessio <ieo7086@cn10.cluster.loc>
2025-05-27 14:57:12 +0200

    updated predict.sh

--- scripts/predict.sh
+++ scripts/predict.sh
@@ -1,18 +1,16 @@
 singularity_imgs_dir=/hpcnfs/scratch/DIMA/chiodin/singularity_images
 scripts_dir=/hpcnfs/scratch/DIMA/chiodin/tests/stardist_training_notebook/stardist_training
-
-# model_name=stardist_e80_spe25_vp0.1_vpp1
-model_name=stardist_e200_spe50_vp0.1_vpp1
-base_dir=/hpcnfs/scratch/DIMA/chiodin/tests/stardist_training_notebook/runs/20250430/models
-file=/hpcnfs/scratch/DIMA/chiodin/tests/stardist_training_notebook/runs/20250430/data/test/images/dapi_28.tif
-outdir=/hpcnfs/scratch/DIMA/chiodin/tests/stardist_training_notebook/predictions
-indir=/hpcnfs/scratch/DIMA/chiodin/tests/stardist_training_notebook/runs/20250430/data/test/masks
+#model_name=stardist_full_e200_lr00001_aug1_seed10_es50p0.001_rlr0.5p50
+models_dir=/hpcnfs/scratch/DIMA/chiodin/tests/stardist_training_notebook/runs/20250514/runs/10/models
+model_name=stardist_fixed_full_e200_lr00001_aug1_seed10_es200p0.001_rlr0.5p200
+test_imgs_dir=/hpcnfs/scratch/DIMA/chiodin/tests/stardist_training_notebook/runs/20250514/test_images
+outdir=/hpcnfs/scratch/DIMA/chiodin/tests/stardist_training_notebook/runs/20250514/predictions
 
 singularity exec \
     -B /hpcnfs \
-    ${singularity_imgs_dir}/stardist_training.sif \
-    python ${scripts_dir}/scripts/predict.py \
+    ${singularity_imgs_dir}/stardist_training_v02.sif \
+    python ${scripts_dir}/scripts/predict2.py \
         --model_name ${model_name} \
-        --base_dir ${base_dir} \
-        --indir ${indir} \
+        --models_dir ${models_dir} \
+        --test_imgs_dir ${test_imgs_dir} \
         --outdir ${outdir}
